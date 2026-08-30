36b48ecbefa98d668d998243763a971af12e1bc6
Akshay-01001 <151864572+Akshay-01001@users.noreply.github.com>
2026-08-30 18:43:35 +0530

    remove chess move model

---
+++ server/prisma/migrations/20260830131224_remove_moves_model/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Move` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Move" DROP CONSTRAINT "Move_gameId_fkey";
+
+-- DropTable
+DROP TABLE "Move";
